16a5f0362836f592ef6c01c7e9aa31cd05cef8ee
Gregor Kasieczka <g.kasieczka@gmail.com>
2016-06-28 10:05:16 +0200

    Added run_daint.sh

---
+++ dnn_template/run_daint.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+echo "Go to scratch"
+cd $SCRATCH
+
+echo "PWD:"
+pwd
+
+echo "Prepare modules"
+module swap PrgEnv-cray/5.2.82 PrgEnv-gnu/5.2.40
+module load Python/2.7.10-CrayGNU-5.2.40
+module load pycuda/2015.1-CrayGNU-5.2.40-Python-2.7.10
+module load h5py/2.5.0-CrayGNU-5.2.40-Python-2.7.10-serial
+module swap cudatoolkit/6.5.14-1.0502.9613.6.1  cudatoolkit/7.0.28-1.0502.10742.5.1
+
+# Enable CUDNN
+export CUDNN_BASE=$SCRATCH/cuda
+export LD_LIBRARY_PATH=$CUDNN_BASE/lib64:$LD_LIBRARY_PATH
+export CPATH=$CUDNN_BASE/include:$CPATH
+export LIBRARY_PATH=$CUDNN_BASE/lib64:$LD_LIBRARY_PATH
+
+# avoid lock-issues 
+export THEANO_FLAGS="base_compiledir=$SCRATCH/theano.NOBACKUP"
+
+cd DeepTop/dnn_template
+
+echo "Starting TrainClassifiers.py"
+python TrainClassifiers.py
+
+echo "Done.."
