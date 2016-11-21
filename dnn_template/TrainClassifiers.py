ca2cc5757d1a7d509572ede8cd70f8b82613913e
Gregor Kasieczka <g.kasieczka@gmail.com>
2016-11-21 22:33:56 +0000

    v7

--- dnn_template/TrainClassifiers.py
+++ dnn_template/TrainClassifiers.py
@@ -50,8 +50,8 @@ default_params = {
     
     # Parameters for 2d convolutional architecture    
     "n_blocks"        : 2,    
-    "n_conv_layers"   : 2,        
-    "conv_nfeat"      : 4,
+    "n_conv_layers"   : 3,        
+    "conv_nfeat"      : 8,
     "conv_size"       : 4,
     "conv_batchnorm"  : 0,
     "pool_size"       : 2,
@@ -499,10 +499,10 @@ classifiers = [
 #               class_names = {0: "background", 1: "signal"}               
 #               ),
 
-    Classifier("NNXd_et_5deg_sample_v3_v6", 
+    Classifier("NNXd_et_5deg_sample_v3_v7", 
                "keras",
                params,
-               True,
+               False,
                datagen_train_pixel,
                datagen_test_pixel,               
                model_2d(params),
