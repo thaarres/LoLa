637efa7ddf4dd77d92f7fc7bce6e0363c1cdc05c
Gregor Kasieczka <g.kasieczka@gmail.com>
2017-01-14 18:51:44 +0100

    v14

--- dnn_template/TrainClassifiers.py
+++ dnn_template/TrainClassifiers.py
@@ -72,10 +72,10 @@ default_params = {
     # Common parameters
     "n_chunks"          : 10,
     "batch_size"        : 1000,
-    "lr"                : 0.0005,
-    "decay"             : 0.0005/600,
-    "momentum"          : 0.9,            
-    "nb_epoch"          : 600,
+    "lr"                : 0.002,
+    "decay"             : 0,
+    "momentum"          : 0,            
+    "nb_epoch"          : 200,
     "samples_per_epoch" : None, # later filled from input files
 }
 
@@ -506,7 +506,7 @@ classifiers = [
 #               class_names = {0: "background", 1: "signal"}               
 #               ),
 
-    Classifier("NNXd_et_5deg_sample_v5_v13_" + SUFFIX, 
+    Classifier("NNXd_et_5deg_sample_v5_v14_" + SUFFIX, 
                "keras",
                params,
                False,
