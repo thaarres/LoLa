453dc9a4a47462380c0a304cdeb177df2c27ad10
Gregor Kasieczka <g.kasieczka@gmail.com>
2017-01-26 14:24:19 +0100

    test v27

--- dnn_template/TrainClassifiers.py
+++ dnn_template/TrainClassifiers.py
@@ -85,8 +85,8 @@ cut_train =  "(entry%2==0)"
 cut_test  =  "(entry%2==1)"
 
 # Reading H5FS
-infname_train = "/scratch/snx3000/gregork/train-img-et-5deg-v7.h5"
-infname_test  = "/scratch/snx3000/gregork/test-img-et-5deg-v7.h5"
+infname_train = "/scratch/snx3000/gregork/train-img-min-5deg-v6.h5"
+infname_test  = "/scratch/snx3000/gregork/test-img-min-5deg-v5-testonly.h5"
 
 
 ########################################
@@ -501,10 +501,10 @@ classifiers = [
 #               class_names = {0: "background", 1: "signal"}               
 #               ),
 
-    Classifier("NNXd_min_5deg_sample_v7_v28_" + SUFFIX, 
+    Classifier("NNXd_min_5deg_sample_v6_v27_" + SUFFIX, 
                "keras",
                params,
-               False,
+               True,
                datagen_train_pixel,
                datagen_test_pixel,               
                model_2d(params),
