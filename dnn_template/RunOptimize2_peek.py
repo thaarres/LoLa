7886b61766338d1fad898489dce7194eb4cf4934
Gregor Kasieczka <g.kasieczka@gmail.com>
2017-06-16 22:11:53 +0200

    l4

--- dnn_template/RunOptimize2_peek.py
+++ dnn_template/RunOptimize2_peek.py
@@ -20,19 +20,15 @@ space= {
     "use_angular_dr" : hp.choice("use_angular_dr", [True,False]),
     "lola_filters"   : hp.quniform("lola_filters", 1, 10, 1),
     "n_lolas"        : hp.quniform("n_lolas", 1, 4, 1),
-    "do_mult_p"      : hp.choice("do_mult_p",   [0,1,2]),
-    "mult_p"         : hp.choice("mult_p",   [-1,0,1]),
-    "regularize_weight" : hp.choice("regularize_weight", [True,False]),
-    "train_regularize_weight" : hp.choice("train_regularize_weight", [True,False]),
 }
 
 
-trials = MongoTrials('mongo://localhost:23888/foo_db/jobs', exp_key='l4')
+trials = MongoTrials('mongo://localhost:23888/foo_db/jobs', exp_key='l3')
 
-#print(len(trials.losses()))
-#print(trials.losses())
-#print(min([float(x) for x in trials.losses() if x]))
-#print(trials.best_trial)
+print(len(trials.losses()))
+print(trials.losses())
+print(min([float(x) for x in trials.losses() if x]))
+print(trials.best_trial)
 
 
 #plt.plot([x if x<1 else 1. for x in trials.losses() if not x is None])
@@ -40,7 +36,7 @@ trials = MongoTrials('mongo://localhost:23888/foo_db/jobs', exp_key='l4')
 #plt.savefig("test.png")
 
 
-best = fmin(main, space, trials=trials, algo=tpe.suggest, max_evals=10000,verbose=1)
+#best = fmin(main, space, trials=trials, algo=tpe.suggest, max_evals=10000,verbose=1)
 
 
 
---
+++ dnn_template/SimpleTrainDaint.py
@@ -0,0 +1,27 @@
+from TrainClassifiers import main
+
+params = {"input_path"              : "/scratch/snx3000/gregork/",
+          "output_path"             : "/scratch/snx3000/gregork/output/",
+          "output_path"             : "./",
+          "inputs"                  : "constit_lola",
+          "model_name"              : "LolaTest_0D",        
+          "n_classes"               : 2,
+          "signal_branch"           : "is_signal_new",
+          "nb_epoch"                : 50,
+          "batch_size"              : 1024,
+          "name_train"              : "topconst-train-v1-resort.h5" ,
+          "name_test"               : "topconst-test-v1-resort.h5",
+          "train_poly"              : False,
+          "train_offset"            : "diag",
+          "train_metric"            : False,
+          "train_minmax"            : False,
+          "use_angular_dr"          : True,
+          "do_mult_p"               : 2,
+          "mult_p"                  : -1,
+          "regularize_weight"       : True,
+          "train_regularize_weight" : True,
+          "lola_filters"            : 1,
+          "n_lolas"                 : 1,
+}
+
+main(params)
