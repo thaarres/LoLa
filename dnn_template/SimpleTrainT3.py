651cc449c3344469d9807f724c5d7682f83d76b8
Gregor Kasieczka <gregor.kasieczka@cern.ch>
2017-06-20 12:11:50 +0200

    t3

---
+++ dnn_template/SimpleTrainT3.py
@@ -0,0 +1,13 @@
+from TrainClassifiers import main
+
+params = {"input_path"              : "./",
+          "output_path"             : "./",
+          "inputs"                  : "constit_lola",
+          "model_name"              : "Lola_Ref1",        
+          "nb_epoch"                : 100,
+          "batch_size"              : 1024,
+          "name_train"              : "topconst-train-v3-resort.h5" ,
+          "name_test"               : "topconst-test-v3-resort.h5",
+}
+
+main(params)
