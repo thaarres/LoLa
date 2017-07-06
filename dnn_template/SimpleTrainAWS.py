9bbcdc4fe3c8d06e2fe6c0e53dd60dc3d08f1c43
Gregor Kasieczka <g.kasieczka@gmail.com>
2017-07-06 08:39:37 +0000

    AWS ref

--- dnn_template/SimpleTrainAWS.py
+++ dnn_template/SimpleTrainAWS.py
@@ -1,16 +1,27 @@
-
 from TrainClassifiers import main
-
 
 params = {"input_path"              : "./",
           "output_path"             : "./",
           "inputs"                  : "constit_lola",
-          "model_name"              : "LoLaTest_Ref1_10const",        
-          "nb_epoch"                : 100,
+          "model_name"              : "Lola_Poly3g",        
+          "nb_epoch"                : 200,
           "batch_size"              : 1024,
-          "name_train"              : "topconst-train-v2-resort.h5" ,
-          "name_test"               : "topconst-test-v2-resort.h5",
-          "n_constit"               : 10,
+          "name_train"              : "topconst-train-v3-resort.h5" ,
+          "name_test"               : "topconst-test-v3-resort.h5",
+
+          "train_poly"                     : True,
+          "train_offset"                   : "full",
+          "train_metric"                   : False,
+          "train_minmax"                   : False,
+          "lola_filters"                   : 1,
+          "use_angular_dr"                 : True,
+          "n_lolas"                        : 1,
+          "do_mult_p"                      : 0,
+          "mult_p"                         : 0,
+          "regularize_weight"              : False,
+          "train_regularize_weight"        : False,
+          "train_regularize_weight_target" : False,
+          "take_diff"                      : True,
 }
 
 main(params)
