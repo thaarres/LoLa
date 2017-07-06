837ef5e7d106298aa70c7e8a8ce0b81cee75ad88
Gregor Kasieczka <g.kasieczka@gmail.com>
2017-07-06 13:15:10 +0000

    thres: 0

--- dnn_template/SimpleTrainAWS.py
+++ dnn_template/SimpleTrainAWS.py
@@ -3,7 +3,7 @@ from TrainClassifiers import main
 params = {"input_path"              : "./",
           "output_path"             : "./",
           "inputs"                  : "constit_lola",
-          "model_name"              : "Lola_AWSRef2",        
+          "model_name"              : "Lola_AWSRef2_thres_0",        
           "nb_epoch"                : 200,
           "batch_size"              : 1024,
           "name_train"              : "topconst-train-v3-resort.h5" ,
