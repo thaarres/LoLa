6829c297e0bfdbdd38268c2057c1a480f3801836
Ubuntu <ubuntu@ip-172-31-10-127.eu-west-1.compute.internal>
2017-07-09 21:29:11 +0000

    minsum x2

--- dnn_template/SimpleTrainAWS.py
+++ dnn_template/SimpleTrainAWS.py
@@ -3,7 +3,7 @@ from TrainClassifiers import main
 params = {"input_path"              : "./",
           "output_path"             : "./",
           "inputs"                  : "constit_lola",
-          "model_name"              : "Lola_AWS_Ref3_min_mij_minmax_dij",        
+          "model_name"              : "Lola_AWS_Ref3_min_mij_x2",        
           "nb_epoch"                : 200,
           "batch_size"              : 1024,
           "name_train"              : "topconst-train-v3-resort.h5" ,
