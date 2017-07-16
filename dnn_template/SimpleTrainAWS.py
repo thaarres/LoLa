c7a0720a8da7c3f4fb71f4cfb299568a6c7cb5ce
Ubuntu <ubuntu@ip-172-31-4-26.eu-west-1.compute.internal>
2017-07-16 19:06:36 +0000

    Ref8_v4

--- dnn_template/SimpleTrainAWS.py
+++ dnn_template/SimpleTrainAWS.py
@@ -3,13 +3,13 @@ from TrainClassifiers import main
 params = {"input_path"              : "./",
           "output_path"             : "./",
           "inputs"                  : "constit_lola",
-          "model_name"              : "Lola_AWS_Ref8",        
+          "model_name"              : "Lola_AWS_Ref8_v4",        
           "nb_epoch"                : 200,
           "batch_size"              : 1024,
-          "n_constit"               : 15,
+          "n_constit"               : 20,
           "n_features"              : 4,
-          "name_train"              : "topconst-train-v3-resort.h5" ,
-          "name_test"               : "topconst-test-v3-resort.h5",
+          "name_train"              : "topconst-train-v4-resort.h5" ,
+          "name_test"               : "topconst-test-v4-resort.h5",
 
 
 }
